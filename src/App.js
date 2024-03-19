833b8cce991da2a1c80b27f8bfa27eab45c7cb87
Adam Mulani <aadammulani12@gmail.com>
2024-03-19 18:03:29 +0530

    If no tours are left then it shows message 'No tours left' with refresh button

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ const App = () => {
     const newTour=tours.filter(tour=>tour.id!==id);
     setTours(newTour);
   }
+
+  if(tours.length===0)
+  {
+    return (
+      <div className="refresh">
+        <h2>No Tours left</h2>
+        <button onClick={()=>setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    )
+  }
   return (
     <div>
     <Tours tours={tours} removeTour={removeTour}></Tours>
